b6ada485ee363ce985d2051f09906e3c24315d76
Jean <jeanperfoll@gmail.com>
2018-12-22 00:04:31 -0200

    Projeto final da disciplina

---
+++ src/main/java/br/edu/unidavi/infrawebtrabalhofinal/InfraWebTrabalhoFinalApplication.java
@@ -0,0 +1,14 @@
+package br.edu.unidavi.infrawebtrabalhofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraWebTrabalhoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraWebTrabalhoFinalApplication.class, args);
+	}
+
+}
+
